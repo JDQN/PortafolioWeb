d374b41a5b51c7f17ac55771b19f3a95d5c44b7e
Juan David Quimbayo Núñez <74111185+JDQN@users.noreply.github.com>
2022-11-25 22:26:10 -0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>Juan David Quimbayo Núñez</h1>
-                <h5 className="text-light">Fulll Stack Developer</h5>
+                <h5 className="text-light">Full Stack Developer</h5>
                 <Cta/> 
                 <HeaderSocial/>
 
